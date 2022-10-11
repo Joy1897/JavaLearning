aef6c49fb0864ecfd2bc5d2b3b257b0540f5817f
Jensen Zhao <jensen.zhao@ringcentral.com>
2022-10-11 11:09:36 +0800

    1.第一个java程序

---
+++ Hello.java
@@ -0,0 +1,14 @@
+/**
+ * 一种特殊的多行注释
+ * @author Jensen Zhao
+ * 2022/10/11
+ */
+
+public class Hello {//类名是Hello
+    public static void main(String[] args){//方法名是main
+        System.out.println("My First Java Programm!");
+        /*
+        多行注释
+         */
+    }//方法定义结束
+}//类定义结束
